849a9ece91102ca4ab17167160899ef154d465b5
rudghks373 <rudghks373@naver.com>
2019-01-24 15:30:20 +0900

    Update Log

--- src/main/java/com/yahait/app/Controller/YahaitController.java
+++ src/main/java/com/yahait/app/Controller/YahaitController.java
@@ -54,14 +54,17 @@ public class YahaitController {
 	@RequestMapping("/LoginAct")
 	public String LoginAct(HttpServletRequest request , HttpServletResponse response) throws IOException {
 	
+		//request.getParameter 파라미터로 직접값을 받아오는 형태
 		String userid = request.getParameter("id").toString();
 		String userpassword = request.getParameter("password").toString();
 		
 		System.out.println("view에서받아온ID"+userid+"===="+"view에서받아온PW"+userpassword);
 		
+		//SQL문 불러오기
 		IDao dao = sqlSession.getMapper(IDao.class);
 		ArrayList<MemberDto> logincehck = dao.logincehck(userid);
 		
+		//경고창 response 객체를 이용하여 PrintWriter 하여 쏴주기
 		if(logincehck.isEmpty()) {
 		System.out.println("아이디가 없습니다");
 		response.setContentType("text/html; charset=UTF-8");
@@ -72,13 +75,15 @@ public class YahaitController {
 		}
 		
 		else{
+		
 		System.out.println("입력결과 DB에 아이디존재");
 		System.out.println("입력결과 DB에 아이디존재");
+		//DB에 아이디가 존재하면 로그인 SQL문 실행(userid값을 넣어서 아이디값 확인)
         MemberDto passwordcheck = dao.logincehck(userid).get(0);
         
         System.out.println("로그인입력 패스워드:"+userpassword);
         System.out.println("DB저장된 패스워드:"+passwordcheck.getPassword().trim());
-
+        //클라이언트 입력값과 데이터베이스에 입력값 일치확인
         if(passwordcheck.getPassword().trim().equals(userpassword)){
         	System.out.println("완벽한로그인");
         	return "Login";
@@ -91,8 +96,38 @@ public class YahaitController {
         }
         
 		}
-		
+		//아직 메인창이 완료안되어 그대로 두기
 		return "Login";
+	}
+	
+	//회원가입창 이동
+	@RequestMapping("/Signup")
+	public String Signup(Model model){
+		return "Signup";
+	}
+	
+	@RequestMapping("/SignupCheck")
+	public String SignupCheck(Model model,HttpSession session, @RequestBody String paramData) throws IOException, ParseException{
+		
+		System.out.println("회원가입 컨트롤러 접속");
+		//클라이언트측에서 날라온 데이터확인
+	    System.out.println("클라이언트전송데이터(JSON):"+paramData);
+	    //JSON객체를 생성하여 키&벨류 값으로 쪼개기
+	    JSONParser parser = new JSONParser(); //–JSON Parser 생성
+	    JSONObject jsonObj = (JSONObject)parser.parse(paramData); //– 넘어온 문자열을 JSON 객체로 변환
+		//JSON데이값을 스트링 객체로 저장
+		String Signupid = jsonObj.get("id").toString();
+		String Signuppassword = jsonObj.get("pass").toString();
+		String Signupphone = jsonObj.get("cellPhone").toString();
+		String Signupname = jsonObj.get("name").toString();
+		String Signupmail = jsonObj.get("mail").toString();
+		
+		//클라이언트측에서 날라온 JSON데이터를 서버측에서 받은 데이터 확인
+		System.out.println("서버측 받은 데이터 ");
+		System.out.println("ID:"+Signupid+"PW:"+Signuppassword+"NAME:"+Signupname+"MAIL:"+Signupmail+
+				"PHONE:"+Signupphone);
+		
+		return "EX";
 	}
 	
 	@RequestMapping("/Main")
@@ -124,36 +159,7 @@ public class YahaitController {
 	public String Shop(Model model){
 		return "Shop";
 	}
-	
-	@RequestMapping("/Signup")
-	public String Signup(Model model){
-		return "Signup";
-	}
-	
-	@RequestMapping("/SignupCheck")
-	public String SignupCheck(Model model,HttpSession session, @RequestBody String paramData) throws IOException, ParseException{
-		
-		System.out.println("회원가입 컨트롤러 접속");
-	
-	    System.out.println("클라이언트전송데이터(JSON):"+paramData);
-	    
-	    JSONParser parser = new JSONParser(); //–JSON Parser 생성
-	    JSONObject jsonObj = (JSONObject)parser.parse(paramData); //– 넘어온 문자열을 JSON 객체로 변환
-		
-		String Signupid = jsonObj.get("id").toString();
-		String Signuppassword = jsonObj.get("pass").toString();
-		String Signupphone = jsonObj.get("cellPhone").toString();
-		String Signupname = jsonObj.get("name").toString();
-		String Signupmail = jsonObj.get("mail").toString();
-		
-		System.out.println("서버측 받은 데이터 ");
-		System.out.println("ID:"+Signupid+"PW:"+Signuppassword+"NAME:"+Signupname+"MAIL:"+Signupmail+
-				"PHONE:"+Signupphone);
-		
-		return "EX";
-	}
-	
-	
+
 	
 	@RequestMapping("/FindPW")
 	public String FindPW(Model model){
